7bb740137d23d59eb63122e02a8d2477a5abbb65
Simon Cull <simoncull@gmail.com>
2024-12-02 12:12:55 +0000

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel(),
+});
